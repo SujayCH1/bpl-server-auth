dc29802760c12978cfa2320832e03aee633df5ae
notstrange13 <mailtoshibasis13@gmail.com>
2026-02-08 09:58:32 +0530

    initial commit

---
+++ src/main/java/org/bpl/mcAuth/McAuth.java
@@ -0,0 +1,13 @@
+x`package org.bpl.mcAuth;
+
+import net.fabricmc.api.ModInitializer;
+
+public class McAuth implements ModInitializer {
+
+    public static final String MOD_ID="mcauth";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
